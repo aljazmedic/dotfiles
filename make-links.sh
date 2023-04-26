1bb5ca0f73a378b8890efe5e20ae0bfb91a1b60a
Aljaz Medic <aljaz.medic@gmail.com>
2023-04-27 00:03:10 +0200

    Tweaks for PC

--- make-links.sh
+++ make-links.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -6,8 +6,7 @@ DOT_FOLDERS="tmux,git,zsh"
 
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
     echo "[+] Folder :: $folder"
-    stow --ignore=README.md --ignore=LICENSE \
-        -t $HOME -D $folder
+    stow --ignore=README.md --ignore=LICENSE  -t $HOME -D $folder
     stow -v -t $HOME $folder
 done
 
--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -9,31 +9,10 @@ alias df='df -h'     # human-readable sizes
 alias free='free -m' # show sizes in MB
 
 # EDITOR
-alias vim="nvim"
-alias svim="sudo vim"
-
-
-# confirm before overwriting something
-alias cp="cp -i"
-alias mv='mv -i'
-alias rm='rm -i'
-
-alias ls='ls --color=tty'
-alias la='ls -lAh'
-alias ll='la -h'
-# Utils
-alias path='echo $PATH | tr -s ":" "\n"'
-alias mkdir="mkdir -p"
-alias c='cd'
-
-
-# easier to read disk
-alias df='df -h'     # human-readable sizes
-alias free='free -m' # show sizes in MB
-
-# EDITOR
-alias vim="nvim"
-alias svim="sudo vim"
+if [ -x "$(command -v nvim)" ]; then
+	alias vim="nvim"
+	alias svim="sudo nvim"
+fi
 
 
 # confirm before overwriting something
@@ -42,5 +21,6 @@ alias svim="sudo vim"
 #alias rm='rm -i'
 
 alias ls='ls --color=tty'
-alias la='ls -lAh'
-alias ll='la -h'
+alias la='ls -la'
+alias ll='ls -lAFh'
+
