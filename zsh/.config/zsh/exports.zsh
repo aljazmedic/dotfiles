6296d73acd42feef22d814bc5777608b33f59d36
Aljaž Medič <aljaz.medic@gmail.com>
2023-04-26 20:20:50 +0000

    Add ssha

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -6,3 +6,8 @@ export DIRSTACKSIZE=5
 export PATH="$HOME/.local/bin":$PATH
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
+
+ssha () {
+    [ -z $SSH_AUTH_SOCK ] && eval `ssh-agent`
+    ssh-add $@
+}
