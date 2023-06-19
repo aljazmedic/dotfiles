10dfe51e5220c82d0c981fb8a5530e7916eb7226
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2023-06-19 17:46:11 +0200

    Update git config

--- make-links.sh
+++ make-links.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOT_FOLDERS="tmux git zsh"
 
-DOT_FOLDERS="tmux,git,zsh"
-
-for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
+for folder in $DOT_FOLDERS; do
     echo "[+] Folder :: $folder"
-    stow --ignore=README.md --ignore=LICENSE --ignore=.gitignore -t $HOME -D $folder
-    stow -v -t $HOME $folder
+    stow -t $HOME $folder 
 done
 
 # Reload shell once installed
-echo "[+] Reloading shell..."
-exec $SHELL -l
+echo "[+] Reload shell to see effects..."
