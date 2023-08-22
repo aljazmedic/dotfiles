6cd466dd6602a634eb1794b98ffba8ced388b3ff
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2023-08-22 22:35:12 +0200

    Wsl flag

--- make-links.sh
+++ make-links.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
+# Check if --wsl in arguments
+WSL=false
+while [ $# -gt 0 ]; do
+    case "$1" in
+    --wsl)
+        echo "[+] WSL mode"
+        WSL=true
+        ;;
+    *)
+        echo "[-] Invalid argument: $1"
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+ALL=$(ls ./stow)
+
 DOT_FOLDERS="tmux git zsh pwninit fonts lf nvm"
+if [ $WSL ]; then
+    DOT_FOLDERS="$DOT_FOLDERS $(echo $ALL | grep -o "wsl-.*")"
+fi
 
 for folder in $DOT_FOLDERS; do
     echo "[+] Folder :: $folder"
