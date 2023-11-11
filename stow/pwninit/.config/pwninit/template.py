36374113850b9b3c2442ee2dd029c040117ed426
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2023-11-11 13:55:20 +0100

    Pwninit update

--- stow/pwninit/.config/pwninit/template.py
+++ stow/pwninit/.config/pwninit/template.py
@@ -14,19 +14,28 @@ def start(argv=[], *a, **kw):
         return process({proc_args}+argv, *a, **kw)
 
 if os.environ['TMUX']:
-    a = ['new-window'] if args.TMUX_NEW else ['split-window', '-h']
-    context.terminal = ['tmux'] + a
+    context.terminal = ['tmux', 'split-window', '-h', '-F', '#{pane_id}', '-P']
 
 context.log_level = 'debug'
 
 ###### START #######
 
 gdbscript = '''
+b * __libc_start_main
 b * main
 c
 '''
 
-# Good luck pwning :)
 p = start()
+ru = lambda a:  p.recvuntil(a)
+r  = lambda a:  p.recv(a)
+sla = lambda a,b: p.sendlineafter(a,b)
+sa = lambda a,b: p.sendafter(a,b)
+sl = lambda a: p.sendline(a)
+s = lambda a: p.send(a)
+
+# Good luck pwning :)
+
+
 
 p.interactive()
